c3ea70c18be9c899ea5c47b64ffd8557c2bd7e14
FabioDiCeglie <dcfabio96@gmail.com>
2022-04-11 17:29:51 +0200

    axios request  fetching characters in redux store

---
+++ src/store/Characters/actions.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { apiUrl } from "../../config/apiClient";
+
+// const charactersLoaded = (data) => ({
+//   type: "homepage/charactersLoaded",
+//   payload: data,
+// });
+
+export const fetchCharacters = () => {
+  return async (dispatch) => {
+    try {
+      const response = await axios.get(`${apiUrl}/character`);
+
+      if (response === null) {
+        throw new Error("Failed to load products from the API");
+      } else {
+        // dispatch(charactersLoaded(response));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,3 +1,6 @@
 import { combineReducers } from "redux";
+import charactersReducer from "../store/Characters/reducer";
 
-export default combineReducers({});
+export default combineReducers({
+  characters: charactersReducer,
+});
