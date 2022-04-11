34b09b5f2c58a3dc67547d88625d458aa708ffc8
FabioDiCeglie <dcfabio96@gmail.com>
2022-04-11 17:52:51 +0200

    Update reducer and define selector

--- src/store/Characters/actions.js
+++ src/store/Characters/actions.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 import { apiUrl } from "../../config/apiClient";
 
-// const charactersLoaded = (data) => ({
-//   type: "homepage/charactersLoaded",
-//   payload: data,
-// });
+const charactersLoaded = (data) => ({
+  type: "homepage/charactersLoaded",
+  payload: data,
+});
 
 export const fetchCharacters = () => {
   return async (dispatch) => {
@@ -12,12 +12,11 @@ export const fetchCharacters = () => {
       const response = await axios.get(
         `${apiUrl}/location/?dimension=Fantasy dimension`
       );
-      console.log(response.data);
 
       if (response === null) {
         throw new Error("Failed to load products from the API");
       } else {
-        // dispatch(charactersLoaded(response));
+        dispatch(charactersLoaded(response.data));
       }
     } catch (error) {
       console.log(error);
--- src/store/Characters/reducer.js
+++ src/store/Characters/reducer.js
@@ -2,6 +2,9 @@ const initialState = null;
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case "homepage/charactersLoaded": {
+      return action.payload;
+    }
     default: {
       return state;
     }
