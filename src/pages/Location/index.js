c074cbdce37492630fa0076b6bb1893a1b77b2ad
FabioDiCeglie <dcfabio96@gmail.com>
2022-04-11 20:39:30 +0200

    fetch characters and check data

--- src/pages/Location/index.js
+++ src/pages/Location/index.js
@@ -1,6 +1,16 @@
 /* eslint-disable import/no-anonymous-default-export */
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { fetchCharacters } from "../../store/CharactersByLocation/actions";
 
 export default () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCharacters());
+  }, [dispatch, fetchCharacters]);
+
   return (
     <div>
       <h4>Characters from location:</h4>
