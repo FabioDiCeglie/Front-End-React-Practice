99684799fd7bd404550a0219c55bf40484bd9a95
FabioDiCeglie <dcfabio96@gmail.com>
2022-04-11 17:19:44 +0200

    creating first page and checking the route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 
+import Homepage from "./pages/Homepage";
+
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" />
+        <Route path="/" element={<Homepage />} />
       </Routes>
     </div>
   );
