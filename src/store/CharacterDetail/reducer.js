37854d396e91c5730a7477948ad136e84579985d
FabioDiCeglie <dcfabio96@gmail.com>
2022-04-12 09:45:52 +0200

    reducer logic

--- src/store/CharacterDetail/reducer.js
+++ src/store/CharacterDetail/reducer.js
@@ -2,6 +2,18 @@ const initialState = null;
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case "characterDetailPage/characterLoaded": {
+      const characterDetail = action.payload;
+
+      const lastSeenEpisode = characterDetail.episode.reduce(function (
+        prev,
+        current
+      ) {
+        return prev.created > current.created ? prev : current;
+      });
+
+      return { ...characterDetail, episode: lastSeenEpisode };
+    }
     default: {
       return state;
     }
