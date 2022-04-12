b1b131b15a5ad5afcadd356d43cadbe7fb70ab37
FabioDiCeglie <dcfabio96@gmail.com>
2022-04-12 08:52:57 +0200

    creating store for episode

---
+++ src/pages/Episode/index.js
@@ -0,0 +1,13 @@
+/* eslint-disable import/no-anonymous-default-export */
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+export default () => {
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <h4>All character by </h4>
+    </div>
+  );
+};
