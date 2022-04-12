75d708f7427e223d4d55b0c5829f76d75d83d884
FabioDiCeglie <dcfabio96@gmail.com>
2022-04-12 09:18:15 +0200

    creating character detail page

---
+++ src/pages/CharacterDetail/index.js
@@ -0,0 +1,17 @@
+/* eslint-disable import/no-anonymous-default-export */
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+export default () => {
+  const dispatch = useDispatch();
+
+  //   if () {
+  //     return "Loading";
+  //   }
+
+  return (
+    <div>
+      <h4>Character Detail</h4>
+    </div>
+  );
+};
