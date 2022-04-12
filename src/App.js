3176cd921c86cb6d544178e780261bfd45c0907c
FabioDiCeglie <dcfabio96@gmail.com>
2022-04-12 08:55:42 +0200

    define action query by episode

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import DimensionPage from "./pages/Dimension";
 import Homepage from "./pages/Homepage";
 import LocationPage from "./pages/Location";
+import EpisodePage from "./pages/Episode";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="/characters/dimension" element={<DimensionPage />} />
         <Route path="/characters/location" element={<LocationPage />} />
+        <Route path="/characters/episode" element={<EpisodePage />} />
       </Routes>
     </div>
   );
---
+++ src/store/CharactersByEpisode/actions.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { apiUrlGraphQl } from "../../config/apiClient";
+
+const GET_CHARACTERS_QUERY_BY_EPISODE = `query 
+{episode (id: 28) 
+{id,name,characters {id,name,status,species,gender,location {id,name}, episode{id,name,created}}}}
+`;
+
+const charactersLoaded = (data) => ({
+  type: "episodePage/charactersLoaded",
+  payload: data,
+});
+
+export const fetchCharacters = () => {
+  return async (dispatch) => {
+    try {
+      const responseGraphQL = await axios.post(`${apiUrlGraphQl}`, {
+        query: GET_CHARACTERS_QUERY_BY_EPISODE,
+      });
+
+      const result = responseGraphQL.data.data;
+
+      if (result === null) {
+        throw new Error("Failed to load products from the API");
+      } else {
+        dispatch(charactersLoaded(result.locations.results));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
